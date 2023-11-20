83cf2fecf149d24b855aa300918ec0e36f9e9f6b
Olvet7 <volya.maslova@gmail.com>
2023-11-20 05:19:23 +0200

    trying to do 10HW

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,52 +1,59 @@
 import axios from "axios";
 
-
-let selectBreed = document.querySelector('.breed-select');
-let allBreeds = [];
+// let selectBreed = document.querySelector('.breed-select');
+// let allBreeds = [];
 
 // Напиши функцію fetchBreeds(), яка виконує HTTP-запит і повертає проміс із масивом порід - результатом запиту. Винеси її у файл cat-api.js та зроби іменований експорт.
 export function fetchBreeds(){
     // https://api.thecatapi.com/v1/breeds?api_key=live_3D2cMhQlEJ7vd6aSoPS7npBO0fxZO1welUEt4RfV4cU4u5uSgA89im82uOpSVcxT:
     const BASE_URL = 'https://api.thecatapi.com/v1';
     const API_KEY = 'live_3D2cMhQlEJ7vd6aSoPS7npBO0fxZO1welUEt4RfV4cU4u5uSgA89im82uOpSVcxT';
+    const ENDPOINT = '/breeds'
     axios.defaults.headers.common['x-api-key'] = API_KEY;
-    return axios.get((`${BASE_URL}/breeds`));
+    return axios.get((`${BASE_URL}/breeds?api_key=${API_KEY}`));
 };
-// Моє
-// fetchBreeds() 
-//   .then((response) => {
-//       const allBreeds = response.data;
-//       console.log(allBreeds);
-//   })
-//   .catch(error => {
-//       close.error('Error fetching breeds:', error);
-//   })
-
-// export function createMarcupBreeds(allBreeds) {
-//   return allBreeds.map(({name}) => {
-//     `<option value='${name}'>'${name}'</option>`
-//   }).join('');
-// }
 
 export function createMarcupBreeds(allBreeds) {
   return allBreeds.map(({ name }) => `<option value="${name}">${name}</option>`).join('');
 }
 
-fetchBreeds() 
-  .then((response) => {
-    const breeds = response.data;
-    const optionsMarkup = breeds.map(breed => `<option value="${breed.id}">${breed.name}</option>`).join('');
-    selectBreed.innerHTML = optionsMarkup;
-  })
-  .catch(error => {
-      console.error('Error fetching breeds:', error);
-  });
+// fetchBreeds() 
+//   .then((response) => {
+//     const breeds = response.data;
+//     const optionsMarkup = breeds.map(breed => `<option value="${breed.id}">${breed.name}</option>`).join('');
+//     selectBreed.innerHTML = optionsMarkup;
+//   })
+//   .catch(error => {
+//       console.error('Error fetching breeds:', error);
+//   });
 
 
 // Напиши функцію fetchCatByBreed(breedId), яка очікує ідентифікатор породи, робить HTTP-запит і повертає проміс із даними про кота - результатом запиту. Винеси її у файл cat-api.js і зроби іменований експорт.
-function fetchCatByBreed(breedId){
+export function fetchCatByBreed(breedId){
+  // https://api.thecatapi.com/v1/images/search?breed_ids=breedId
+  const BASE_URL = 'https://api.thecatapi.com/v1';
+  const API_KEY = 'live_3D2cMhQlEJ7vd6aSoPS7npBO0fxZO1welUEt4RfV4cU4u5uSgA89im82uOpSVcxT';
+  const ENDPOINTCAT = '/images/search' 
   
+  axios.defaults.headers.common['x-api-key'] = API_KEY;
+  return axios.get(`${BASE_URL}${ENDPOINTCAT}?breed_ids=${breedId}`)
+  .then(response => response.data)
+  .catch(error => {
+    throw error;
+  });
 };
 
-// Звідси скопіювати і спробувати внести в код 
-// https://codepen.io/adenforshaw/pen/xxYJKVQ
+// Функція відмальовує в дом інформацію про кота
+export function createMarcupCat(data){
+  const { url } = data[0];
+  const { name, description, temperament } = data[0].breeds[0];
+
+  return `<div class="cat">
+  <img src="${url}" alt="${name}" height="300">
+  <div class="cat-info">
+    <h1 class="titleName">${name}</h1>
+    <p class="description">${description}</p>
+    <p><span class="temperament">Temperament:</span> ${temperament}</p>
+  </div>
+</div>`
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,42 @@
 import axios from 'axios';
 import SlimSelect from 'slim-select';
 import {fetchBreeds} from './cat-api';
+import {fetchCatByBreed} from './cat-api';
+import {createMarcupCat} from './cat-api';
 
+const refs = {
+  selectBreed: document.querySelector('.breed-select'),
+  selectEl: document.querySelector('.breed-select'),
+  loaderEl: document.querySelector('.loader'),
+  errorEl: document.querySelector('.error'),
+  divEl: document.querySelector('.cat-info')
+}
 
+showLoader();
+hideError() 
+
+fetchBreeds() 
+  .then((response) => {
+    const breeds = response.data;
+    const optionsMarkup = breeds.map(breed => `<option value="${breed.id}">${breed.name}</option>`).join('');
+    refs.selectBreed.innerHTML = optionsMarkup;
+  })
+  .catch(error => {
+      console.error('Error fetching breeds:', error);
+  })
+  .finally(() => {
+    hideLoader(); // Ховаємо loader після завершення запиту
+  });
+
+// refs.selectEl.addEventListener('change', (evt) => {
+//   const breedId = evt.target.value;
+//   fetchCatByBreed(breedId) {
+//     .then(({name}) => {
+//       // Обробка отриманих даних про кота
+//       // Відображення інформації про кота у div.cat-info
+//     })
+//   }
+// });
 
 // Ваша реалізація функцій з cat-api.js
 
@@ -27,7 +61,7 @@ function hideError() {
 }
 
 // Завантаження списку порід під час завантаження сторінки
-document.addEventListener('DOMContentLoaded', async () => {
+refs.selectEl.addEventListener('DOMContentLoaded', async () => {
   try {
     showLoader();
     const response = await fetchBreeds();
@@ -43,16 +77,53 @@ document.addEventListener('DOMContentLoaded', async () => {
 });
 
 // Обробник подій для вибору породи
-document.querySelector('.breed-select').addEventListener('change', async (event) => {
-  const selectedBreedId = event.target.value;
-  try {
-    showLoader();
-    const catInfo = await fetchCatByBreed(selectedBreedId);
-    // Відобразіть інформацію про кота в DOM
-    // Наприклад, ви можете викликати функцію, яка створить блок із зображенням та іншою інформацією
-  } catch (error) {
-    showError();
-  } finally {
-    hideLoader();
-  }
-});
+
+refs.selectEl.addEventListener('change', onShowCat);
+showLoader(); 
+
+function onShowCat(evt) {
+  const breedId = evt.target.value;
+  fetchCatByBreed(breedId)
+  .then(data => {
+    const markupCat = createMarcupCat(data)
+    refs.divEl.innerHTML = markupCat;
+  })
+  .catch(error => {
+    console.error('Error fetching cat data:', error);
+  })
+  .finally(() => {
+    hideLoader(); // Ховаємо loader після завершення запиту
+  });
+}
+
+// const breedId = event.target.value;
+  // fetchCatByBreed(breedId)
+
+  // try {
+  //   console.log('ololo');
+  //   showLoader();
+  //   const catInfo = await fetchCatByBreed(breedId);
+  //   const catMarkup = createMarcupCat(catInfo)
+  //   refs.divEl.innerHTML = catMarkup;
+
+  //   // Відобразіть інформацію про кота в DOM - назва породи, опис і темперамент
+  //   // Наприклад, ви можете викликати функцію, яка створить блок із зображенням та іншою інформацією
+  // } catch (error) {
+  //   showError();
+  // } finally {
+  //   hideLoader();
+  // }
+// });
+
+// Функція відмальовує в дом інформацію про кота
+export function createMarcupCat(data){
+  const {name, wikipedia_url, temperament, url} = data;
+  return catData = `<div class="cat">
+  <img src="${url}" alt="${name}">
+  <div>
+    <h1 class="titleName">${name}</h1>
+    <p class="description">${wikipedia_url}</p>
+    <p class="temperament">${temperament}</p>
+  </div>
+</div>`
+}
